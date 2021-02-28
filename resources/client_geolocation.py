21b864d542b63d8bc434222989bba92cab7cdb13
Joanna Nowakowska <joanna.k.nowakowska@gmail.com>
2021-02-28 19:00:05 +0100

    Postman collection updated (Geolocation_RESTful_API.postman_collection.json)

--- resources/client_geolocation.py
+++ resources/client_geolocation.py
@@ -1,11 +1,11 @@
 from flask_jwt_extended import jwt_required
-from flask_restx import Resource
 from flask import request
 
 from external_api import get_location_data
+from resources.geolocation import Geolocation
 
 
-class ClientGeolocation(Resource):
+class ClientGeolocation(Geolocation):
     """This class is called with /client_geolocation endpoint."""
 
     @jwt_required()
@@ -13,13 +13,15 @@ class ClientGeolocation(Resource):
         """Detect client's location based on its IP and save it to the 'geolocations' table."""
 
         if request.headers.getlist("X-Forwarded-For"):
-            client_ip = request.headers.getlist("X-Forwarded-For")[0]
+            self.ip_of_interest = request.headers.getlist("X-Forwarded-For")[0]
         else:
-            client_ip = request.remote_addr
+            self.ip_of_interest = request.remote_addr
 
         try:
-            location_info = get_location_data(client_ip)
+            location_info = get_location_data(self.ip_of_interest)
         except Exception as e:
             return self.external_API_error(e)
+
+        self.url_of_interest = None
 
         return self.save_to_db(location_info)
