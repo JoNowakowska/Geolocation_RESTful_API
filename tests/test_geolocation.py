5015ecbac27420e8e27a806c213de8f064631b66
Joanna Nowakowska <joanna.k.nowakowska@gmail.com>
2021-02-28 18:04:04 +0100

    Tests for /geolocation endpoint updated

--- tests/test_geolocation.py
+++ tests/test_geolocation.py
@@ -24,7 +24,7 @@ class TestRecipeResource(BaseTest):
                                        )
                 self.access_token = json.loads(response.data)['access_token']
 
-    def test_get_location_success(self):
+    def test_get_all_from_db_success_empty(self):
         with self.app_context():
             with self.app() as client:
                 response = client.get(f"{URL}/geolocation",
@@ -34,11 +34,11 @@ class TestRecipeResource(BaseTest):
         expected = {"All records saved to db": []}
 
         self.assertDictEqual(json.loads(response.data), expected,
-                             "Message returned after successfully saving client's ip is incorrect.")
+                             "Message returned when showing all records from empty db is incorrect.")
         self.assertEqual(response.status_code, 200,
-                         "Status code returned after successfully saving client's ip is incorrect.")
+                         "Status code returned when showing all records from empty db is incorrect.")
 
-    def test_get_location_already_exists(self):
+    def test_get_all_from_db_success_not_empty(self):
         with self.app_context():
             with self.app() as client:
                 new_geo = Geolocations(ip="127.0.0.1")
@@ -48,45 +48,48 @@ class TestRecipeResource(BaseTest):
                                       headers={"Authorization": f"Bearer {self.access_token}"}
                                       )
 
-                expected = {"message": "IP already exists in db.",
-                            "IP data": {
-                                "id": 1,
-                                "ip": "127.0.0.1",
-                                "ipv_type": None,
-                                "continent_code": None,
-                                "continent_name": None,
-                                "country_code": None,
-                                "country_name": None,
-                                "region_code": None,
-                                "region_name": None,
-                                "city": None,
-                                "zip": None,
-                                "latitude": None,
-                                "longitude": None
-                            }
-                            }
+                expected = {"All records saved to db": [
+                    {
+                        "id": 1,
+                        "ip": "127.0.0.1",
+                        "url": None,
+                        "ipv_type": None,
+                        "continent_code": None,
+                        "continent_name": None,
+                        "country_code": None,
+                        "country_name": None,
+                        "region_code": None,
+                        "region_name": None,
+                        "city": None,
+                        "zip": None,
+                        "latitude": None,
+                        "longitude": None
+                    }
+                ]
+                }
 
                 self.maxDiff = None
 
                 self.assertDictEqual(json.loads(response.data), expected,
-                                     "Message returned after trying to save not unique client's ip is incorrect.")
+                                     "Message returned when when showing all records from not empty db is incorrect.")
                 self.assertEqual(response.status_code, 200,
-                                 "Status code returned after trying to save not unique client's ip is incorrect.")
+                                 "Status code returned when showing all records from not empty db is incorrect.")
 
-    def test_post_location_success(self):
+    def test_post_location_by_ip_success(self):
         with self.app_context():
             with self.app() as client:
                 response = client.post(f"{URL}/geolocation",
-                                      headers={
-                                          "Content-Type": "application/json",
-                                          "Authorization": f"Bearer {self.access_token}"},
-                                      data=json.dumps({"ip_address": "134.201.250.155"})
-                                      )
+                                       headers={
+                                           "Content-Type": "application/json",
+                                           "Authorization": f"Bearer {self.access_token}"},
+                                       data=json.dumps({"ip": "134.201.250.155"})
+                                       )
 
                 expected = {"message": "Successfully saved to db",
                             "IP data": {
                                 "id": 1,
                                 "ip": "134.201.250.155",
+                                "url": None,
                                 "ipv_type": "ipv4",
                                 "continent_code": "NA",
                                 "continent_name": "North America",
@@ -98,7 +101,7 @@ class TestRecipeResource(BaseTest):
                                 "zip": "90012",
                                 "latitude": "34.0655517578125",
                                 "longitude": "-118.240539550781"
-                                }
+                            }
                             }
 
                 self.maxDiff = None
@@ -107,3 +110,112 @@ class TestRecipeResource(BaseTest):
                                      "Message returned after successfully saving ip of interest is incorrect.")
                 self.assertEqual(response.status_code, 201,
                                  "Status code returned after successfully saving ip of interest is incorrect.")
+
+    def test_post_location_by_url_success(self):
+        with self.app_context():
+            with self.app() as client:
+                response = client.post(f"{URL}/geolocation",
+                                       headers={
+                                           "Content-Type": "application/json",
+                                           "Authorization": f"Bearer {self.access_token}"},
+                                       data=json.dumps({"url": "google.com"})
+                                       )
+
+                expected = {"message": "Successfully saved to db",
+                            "IP data": {
+                                "id": 1,
+                                "ip": "142.250.185.174",
+                                "url": "google.com",
+                                "ipv_type": "ipv4",
+                                "continent_code": "NA",
+                                "continent_name": "North America",
+                                "country_code": "US",
+                                "country_name": "United States",
+                                "region_code": "CA",
+                                "region_name": "California",
+                                "city": "Mountain View",
+                                "zip": "94043",
+                                "latitude": "37.4191589355469",
+                                "longitude": "-122.075408935547"
+                            }
+                            }
+
+                self.maxDiff = None
+
+                self.assertDictEqual(json.loads(response.data), expected,
+                                     "Message returned after successfully saving url of interest is incorrect.")
+                self.assertEqual(response.status_code, 201,
+                                 "Status code returned after successfully saving url of interest is incorrect.")
+
+    def test_post_location_by_ip_url_fail(self):
+        with self.app_context():
+            with self.app() as client:
+                response = client.post(f"{URL}/geolocation",
+                                       headers={
+                                           "Content-Type": "application/json",
+                                           "Authorization": f"Bearer {self.access_token}"},
+                                       data=json.dumps({"ip": "134.201.250.155",
+                                                        "url": "google.com"})
+                                       )
+
+                expected = {
+                    "message": "Bad request",
+                    "IP data": "To learn IP information, provide either url or ip in the body as JSON, not both!"
+                }
+
+                self.assertDictEqual(json.loads(response.data), expected,
+                                     "Message returned after request failed due to providing both id and url incorrect.")
+                self.assertEqual(response.status_code, 400,
+                                 "Status code returned after request failed due to providing both id and url incorrect.")
+
+    def test_delete_success(self):
+        with self.app_context():
+            with self.app() as client:
+                new_geo = Geolocations(id=1,
+                                       ip="134.201.250.155",
+                                       url=None,
+                                       ipv_type="ipv4",
+                                       continent_code="NA",
+                                       continent_name="North America",
+                                       country_code="US",
+                                       country_name="United States",
+                                       region_code="CA",
+                                       region_name="California",
+                                       city="Los Angeles",
+                                       zip="90012",
+                                       latitude="34.0655517578125",
+                                       longitude="-118.240539550781"
+                                       )
+                new_geo.add_to_session()
+                db.session.commit()
+
+                response = client.delete(f"{URL}/geolocation",
+                                         headers={
+                                             "Content-Type": "application/json",
+                                             "Authorization": f"Bearer {self.access_token}"},
+                                         data=json.dumps({"ip": "134.201.250.155"})
+                                         )
+
+                expected = {"message": "Item with the ip=134.201.250.155 and url=None deleted successfully!."}
+
+                self.assertDictEqual(json.loads(response.data), expected,
+                                     "Message returned after request successfully deleted a record by its id incorrect.")
+                self.assertEqual(response.status_code, 200,
+                                 "Status code returned after request successfully deleted a record by its id incorrect.")
+
+    def test_delete_not_exists(self):
+        with self.app_context():
+            with self.app() as client:
+                response = client.delete(f"{URL}/geolocation",
+                                         headers={
+                                             "Content-Type": "application/json",
+                                             "Authorization": f"Bearer {self.access_token}"},
+                                         data=json.dumps({"ip": "134.201.250.155"})
+                                         )
+
+                expected = {"message": "Item with the ip=134.201.250.155 and url=None not found in the db."}
+
+                self.assertDictEqual(json.loads(response.data), expected,
+                                     "Message returned after request failed to delete a non-existing record from db by ip incorrect.")
+                self.assertEqual(response.status_code, 404,
+                                 "Status code returned after request failed to delete a non-existing record from db by ip incorrect.")
